905d4dcd3c0e1b86acad3ec89427de9c7c3cc74e
tsangz2013 <31334117+tsangz2013@users.noreply.github.com>
2017-09-02 15:25:25 -0400

    Add files via upload

---
+++ lc_659.cpp
@@ -0,0 +1,50 @@
+// permenant libs
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+/* problem description & site:
+
+659. Split Array into Consecutive Subsequences
+https://leetcode.com/problems/split-array-into-consecutive-subsequences/description/
+
+You are given an integer array sorted in ascending order (may contain duplicates), 
+you need to split them into several subsequences, where each subsequences consist of at least 3 consecutive integers.
+Return whether you can make such a split.
+
+Example 1:
+Input: [1,2,3,3,4,5]
+Output: True
+Explanation:
+You can split them into two consecutive subsequences :
+1, 2, 3
+3, 4, 5
+Example 2:
+Input: [1,2,3,3,4,4,5,5]
+Output: True
+Explanation:
+You can split them into two consecutive subsequences :
+1, 2, 3, 4, 5
+3, 4, 5
+Example 3:
+Input: [1,2,3,4,4,5]
+Output: False
+Note:
+The length of the input is in range of [1, 10000]
+*/
+
+// problem assigned libs
+#include <vector>
+
+// add libs
+
+
+class Solution {
+public:
+
+	static void test() {
+		assert(true);
+	}
+
+
+};
